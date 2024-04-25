5b6852d8ff55b2fd975922d772d56f5eea22c155
Tejobhasa <tejobhasa.mm@gmail.com>
2024-04-25 14:46:48 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// TODO: Write Your JavaScript Code Here
+function checkEqual (v1, v2){
+    if (v1 === v2){
+        console.log("They are equal in types and values");
+    } else if (v1 == v2){
+        console.log("They are equal in values");
+    } else {
+        console.log("They are Not equal");
+    }
+};
+checkEqual(100, 100);
